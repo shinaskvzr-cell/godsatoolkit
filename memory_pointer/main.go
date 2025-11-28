cb1120921711b6d8103dfbe73ef5764adc6215c4
shinaskvzr-cell <shinaskvzr@gmail.com>
2025-11-28 15:46:06 +0530

    memory_pointer

---
+++ memory_pointer/main.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"day1/maxArray"
+	"day1/modifyVariable"
+	"day1/sliceMake"
+)
+
+func main(){
+	maxArray.MaxInArray()
+	modifyVariable.ModifyVariable()
+	sliceMake.SliceMake()
+}
